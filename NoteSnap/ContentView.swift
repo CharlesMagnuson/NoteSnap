07c82377021922cbd00719f97350d3cb005b7f43
Charles Magnuson <44074187+CharlesMagnuson@users.noreply.github.com>
2026-02-11 01:37:31 -0800

    Initial Commit

---
+++ NoteSnap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteSnap
+//
+//  Created by Charles Magnuson on 2/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoteSnap/NoteSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteSnapApp.swift
+//  NoteSnap
+//
+//  Created by Charles Magnuson on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct NoteSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
